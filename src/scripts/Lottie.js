d25f8a222cb3f96f1591a577a5a2db2f8c23fc2b
Jean-François Leblanc <jfleblanc@jfleblanc.com>
2023-09-14 22:36:11 -0400

    New base

--- src/scripts/Lottie.js
+++ src/scripts/Lottie.js
@@ -1,20 +1,12 @@
-import lottieWeb from 'lottie-web';
-
 export default class Lottie {
   constructor() {
     this.init();
   }
 
-  // Défi: Créer une composante Lottie qui, avec un attribut data, permet de spécifier
-  // le path json à charger
   init() {
-    const animation = lottieWeb.loadAnimation({
-      container: document.querySelector('.animation'),
-      path: 'assets/lottie/demo.json',
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      name: 'Demo Animation',
-    });
+    // Votre code pourrait commencer comme ceci
+    // const animation = lottieWeb.loadAnimation({
+    //   ..
+    // });
   }
 }
